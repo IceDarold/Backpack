568551549968ea3a44e86563373857ee14007b51
Youggood <ymnik50s@gmail.com>
2024-10-30 11:02:41 +0300

    add base item settings

---
+++ Assets/Scripts/ItemScriptableObject.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    [CreateAssetMenu(fileName = "GameItems/Item",  menuName = "GameItems/Item")]
+    public class ItemScriptableObject : ScriptableObject
+    {
+        public Sprite Icon;
+        public string Name;
+        public ItemType ItemType;
+        public WeaponSettings WeaponSettings;
+        public Vector2Int ExtensionSize;
+        
+    }
+
+    [Serializable]
+    public struct WeaponSettings
+    {
+        public int MinDamage;
+        public int MaxDamage;
+        public float ReloadingTime;
+        [Range(0f, 1f)]
+        public float Accuracy;
+        public float Stamina;
+    }
+}
---
+++ Assets/Scripts/ItemScriptableObjectCustomInspector.cs
@@ -0,0 +1,35 @@
+﻿
+using UnityEditor;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    [CustomEditor(typeof(ItemScriptableObject))]
+    public class ItemScriptableObjectCustomInspector : Editor
+    {
+        public override void OnInspectorGUI ()
+        {
+            serializedObject.Update();
+            var so = target as ItemScriptableObject;
+            var weaponProperty = serializedObject.FindProperty("WeaponSettings");
+
+            so.Icon = EditorGUILayout.ObjectField("Icon",so.Icon,typeof(Sprite),allowSceneObjects : false) as Sprite;
+            so.Name = EditorGUILayout.TextField("Name",so.Name);
+            so.ItemType = (ItemType)EditorGUILayout.EnumPopup("Item Type",so.ItemType);
+            if(so.ItemType == ItemType.Weapon)
+            {
+                
+                EditorGUILayout.PropertyField(weaponProperty);
+                
+            }
+            else if(so.ItemType == ItemType.InventoryExtension)
+            {
+                so.ExtensionSize = EditorGUILayout.Vector2IntField("Extension Size", so.ExtensionSize);
+            }
+
+
+
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
